b4e4ba43af4f5c9cf83322f1f56cebfd1cc2a609
Bondok6 <kirolousbondok6@gmail.com>
2022-03-24 11:14:17 +0200

    Add three tables
    vets, specializations, and visits

--- schema.sql
+++ schema.sql
@@ -40,3 +40,32 @@ ADD species_id INT REFERENCES species(id);
 -- Add column owner_id which is a foreign key referencing the owners table
 ALTER TABLE animals
 ADD owner_id INT REFERENCES owners(id);
+
+-- Create a table named vets.
+CREATE TABLE vets (
+	id INT GENERATED ALWAYS AS IDENTITY,
+	name VARCHAR(25),
+	age INT,
+	date_of_graduation DATE,
+	PRIMARY KEY(id)
+);
+
+-- Create a "join table" called specializations.
+CREATE TABLE specializations (
+	vets_id INT NOT NULL,
+	species_id INT NOT NULL,
+	FOREIGN KEY (vets_id) REFERENCES vets (id) ON UPDATE CASCADE,
+	FOREIGN KEY (species_id) REFERENCES species (id) ON UPDATE CASCADE,
+	PRIMARY KEY (vets_id, species_id)
+);
+
+-- Create a "join table" called visits to handle this relationship.
+CREATE TABLE visits (
+	id INT GENERATED ALWAYS AS IDENTITY,
+	visit_date DATE NOT NULL,
+	vets_id INT NOT NULL,
+	animals_id INT NOT NULL,
+	FOREIGN KEY (vets_id) REFERENCES vets (id) ON UPDATE CASCADE,
+	FOREIGN KEY (animals_id) REFERENCES animals (id) ON UPDATE CASCADE,
+	PRIMARY KEY (id, vets_id, animals_id)
+);
